e638f02360ca25cbb8eebe5718ea38bde968e186
Rodrigo Bossini <professorbossini@gmail.com>
2021-08-13 18:12:32 -0300

    Terminamos a apostila 05

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import Pedido from './Pedido'
+import Cartao from './Cartao'
+import Feedback from './Feedback'
+
+const App = () => {
+
+    const componenteFeedback = <Feedback funcaoOK={() => alert("Agradecemos a confirmação")} textoOK="Já foi entregue" funcaoNOK={() => alert ("Vamos verificar")} textoNOK="Ainda não foi entregue"/>
+    return (
+        <div className="container border mt-2">
+            
+            <div className="row border-bottom m-2">
+                <h1 className="display-5 text-center">Seus pedidos</h1>
+            </div>
+
+            <div className="row justify-content-center">
+                <div className="col-sm-8 col-md-6 m-2">
+                    <Cartao cabecalho="22/04/2021">
+                        <Pedido icone="fas fa-hdd fa-2x" titulo="SSD" descricao="SSD Kingston A400 - SATA"/>
+                        {componenteFeedback}
+                    </Cartao>
+                </div>
+            </div>
+
+            <div className="row justify-content-center">
+                <div className="col-sm-8 col-md-6 m-2">
+                    <Cartao cabecalho="20/04/2021">
+                        <Pedido icone="fas fa-book fa-2x" titulo="Livro" descricao="Concrete Mathematics - Donald Knuth" />
+                        {componenteFeedback}
+                    </Cartao>
+                </div>
+            </div>
+
+            <div className="row justify-content-center">
+                <div className="col-sm-8 col-md-6 m-2">
+                    <Cartao cabecalho="21/02/2021">
+                        <Pedido descricao="Notebook Dell - 8Gb - i5" icone="fas fa-laptop fa-2x" titulo="Notebook"/>
+                        {componenteFeedback}
+                    </Cartao>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <App />,
+    document.querySelector('#root')
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
